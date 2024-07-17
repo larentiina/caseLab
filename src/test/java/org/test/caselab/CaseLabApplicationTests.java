800c528913c241d06e375eb048d011471ea9a99c
larentina <118663828+larentiina@users.noreply.github.com>
2024-07-17 22:34:37 +0300

    Initialize

---
+++ src/test/java/org/test/caselab/CaseLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.caselab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
