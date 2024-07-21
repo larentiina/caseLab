69d5424edb472c87c1005bfbcc048c40dc87506b
larentina <118663828+larentiina@users.noreply.github.com>
2024-07-21 21:54:12 +0300

    change db

--- src/main/java/org/test/caselab/controller/FileController.java
+++ src/main/java/org/test/caselab/controller/FileController.java
@@ -12,22 +12,22 @@ import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/files")
 public class FileController {
 
     private final FileService fileService;
 
-    @PostMapping
+    @PostMapping(value = "/files")
     public ResponseEntity<?> createFile(@RequestBody FileDto fileDto){
+
         try {
             Long fileId = fileService.save(fileDto);
             return new ResponseEntity<>(fileId, HttpStatus.CREATED);
 
         }catch (InvalidFileDataException e){
-            return  new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return  new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
         }
     }
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/files/{id}")
     public ResponseEntity<FileDto> createFile(@PathVariable Long id){
         try {
             FileDto fileDto = fileService.findFileById(id);
